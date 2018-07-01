5c73ded73d1a79ef10f109cc0856df648042b1b2
Saujanya Nagpal <saujanyanagpal104@gmail.com>
2018-07-01 13:16:17 +0530

    starter files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Hello</p>
+        <h1>Yo!</h1>
+      </div>
+    );
+  }
+}
+export default App;
